46b2603774e5196ff699744d1494ff8edcf3f53d
Rizee24 <112611806+Rizee24@users.noreply.github.com>
2022-09-22 14:48:40 +0700

    Add files via upload

---
+++ Rizky Arrasyid_2217051086_Praktest2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int angka;
+	cin>>angka;
+	
+	if(angka==1){
+		cout<<"satu\n";
+	}else if(angka==2){
+		cout<<"dua\n";
+	}else if(angka==3){
+		cout<<"tiga\n";
+	}else if(angka>3){
+		cout<<"bilangan lebih besar dari tiga";
+	}
+	
+	
+	
+	
+	return 24;
+}
